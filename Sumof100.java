5fffe28d52d7bfffd038c1b841aeb31e05acc2c2
arunmurugan31 <161407191+arunmurugan31@users.noreply.github.com>
2024-02-28 22:32:58 +0530

    Add files via upload

---
+++ Sumof100.java
@@ -0,0 +1,11 @@
+class Sumof100 {
+    public static void main(String[] args) {
+        int n= 100; int sum=0;
+        for(int i=1;i<=n;i++){
+            sum = sum+i;
+           
+            
+        }
+        System.out.println("Total of first 100 numbers:"+sum);
+    }
+}
