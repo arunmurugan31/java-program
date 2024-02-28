d11e6f5587e52d3f391fbd8ed2cfd41354c7d70f
arunmurugan31 <161407191+arunmurugan31@users.noreply.github.com>
2024-02-28 22:30:42 +0530

    Add files via upload

---
+++ OddCount.java
@@ -0,0 +1,12 @@
+public class OddCount{
+public static void main(String[] args){
+int n=123456789;
+int odd_count=0;
+while(n>0){
+int rem=n%10;
+if(n%2!=0){
+odd_count++;
+}
+n=n/10;}
+System.out.println("The odd count is:" +odd_count);
+}}
