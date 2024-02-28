9db2fac95953c2196bc447f8b7f27017bba0f2e6
arunmurugan31 <161407191+arunmurugan31@users.noreply.github.com>
2024-02-28 22:35:15 +0530

    Add files via upload

---
+++ UsingBreak.java
@@ -0,0 +1,11 @@
+class UsingBreak{
+public static void main(String[] args) {
+for(int i=1;i<=4;i++){
+if(i==5){
+         break;
+           }
+        System.out.println(i);
+       }
+        
+    }
+}
